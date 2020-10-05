b2488e9fb68668f3bf3d2e66666da2338cc3901d
haemtim <71187051+haemtim@users.noreply.github.com>
2020-10-05 17:41:41 +0200

    Add files via upload

---
+++ install-touch-gestures.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+sudo gpasswd -a tim input
+
+sudo apt-get install wmctrl python3 python3-setuptools xdotool python3-gi libinput-tools python-gobject
+
+cd Downloads/
+
+git clone https://github.com/bulletmark/libinput-gestures.git
+
+cd libinput-gestures
+
+sudo make install
+
+libinput-gestures-setup autostart
+
+libinput-gestures-setup start
+
+cd ..
+
+git clone https://gitlab.com/cunidev/gestures.git
+
+cd gestures 
+
+sudo python3 setup.py install
+
+
